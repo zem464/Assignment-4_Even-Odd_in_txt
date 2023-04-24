6c91b5ba6b2b6cb92d8d94636cece65c370db1a9
Iris Membrere <irismembrere@gmail.com>
2023-04-24 09:58:02 +0800

    Create and open text files

--- Assignment-4_Even-Odd_in_txt.py
+++ Assignment-4_Even-Odd_in_txt.py
@@ -6,3 +6,6 @@
 
 # Create a method to process the text files
 def integers():
+    # Create a text file named for the 20 integers, even integers, and odd integers
+    with open("numbers.txt") as numbers_input, open("even.txt", "a") as even_num, open("odd.txt", "a") as odd_num:
+        
